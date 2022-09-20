19fa6ad9e5176c32a8efdfb9e023aff125d3df69
unknown <rockcena483@gmail.com>
2022-09-20 17:32:23 +0530

    commit 2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors =require('cors');
 const mongoose = require("mongoose");
 const dotenv=require("dotenv");
+const e = require('express');
 
 const app =express();
 app.use(cors());
@@ -18,6 +19,8 @@ app.get("/",(req,res)=>{
     res.send("Working")
 })
 
+
+
 const vehicleListSchema=new mongoose.Schema(
     {
         type : String,
@@ -25,23 +28,100 @@ const vehicleListSchema=new mongoose.Schema(
         date : String,
         toll : String,
         tariff : String,
-        dateFormated:String
+        dateFormated:Number
     }
 );
 
 const VehicleList =mongoose.model("VehicleList",vehicleListSchema);
 
-const sample={
-    "type": "vehicleType",
-    "number": "vehicleNumber",
-    "date": "dateTime",
-    "toll": "tollName", 
-    "tariff": "tariff",
-    "dateFormated":"dateFormated"}
+const tollListSchema=new mongoose.Schema(
+    {
+        name:String,
+        carSingle:Number,   
+        carReturn:Number,
+        lcvSingle:Number,
+        lcvReturn:Number,
+        truckSingle:Number,
+        truckReturn:Number,
+        heavySingle:Number,
+        heavyReturn:Number
+    }
+);
 
-        VehicleList.insertMany(sample,(err)=>{
-            if(err) console.log(err);
-            else    console.log("Inserted");
-        })
+const TollList =mongoose.model("TollList",tollListSchema);
+
+
+
+// const sample={
+//     "type": "vehicleType",
+//     "number": "vehicleNumber",
+//     "date": "dateTime",
+//     "toll": "tollName", 
+//     "tariff": "tariff",
+//     "dateFormated":"dateFormated"}
+
+//         VehicleList.insertMany(sample,(err)=>{
+//             if(err) console.log(err);
+//             else    console.log("Inserted");
+//         })
+
+
+app.get("/getVehicleList",(req,res)=>{
+    VehicleList.find({}, null, {sort: {date: -1}},(err,vehicles)=>{
+        res.send(vehicles);
+    })
+})
+
+app.get("/getVehicleListWithfilter/:value",(req,res)=>{
+    let filterValue=req.params.value;
+    console.log("hello");
+    VehicleList.find({number:{$regex: filterValue}},null, {sort: {date: -1}},(err,vehicles)=>{
+        res.send(vehicles);
+    })
+})
+
+
+app.get("/getVehicleListWithfilter2/:value",(req,res)=>{
+    let filterValue=req.params.value;
+    VehicleList.find({type:{$regex:filterValue}},null, {sort: {date: -1}},(err,vehicles)=>{
+        res.send(vehicles);
+    })
+})
+
+app.get("/getTollListWithfilter/:value",(req,res)=>{
+    let filterValue=req.params.value;
+    TollList.find({name:{$regex:filterValue}},null, {sort: {date: -1}},(err,tolls)=>{
+        res.send(tolls);
+    })
+})
+
+app.post("/postVehicle",(req,res)=>{
+    VehicleList.insertMany([req.body],(err)=>{
+        if(err) console.log(err);
+        else    console.log("inserted");
+    })
+})
+
+app.get("/getTollList",(req,res)=>{
+    TollList.find({},null,{sort: {name: 1}}, (err,tolls)=>{
+        res.send(tolls);
+    })
+})
+
+app.post("/postToll",(req,res)=>{
+    TollList.insertMany([req.body],(err)=>{
+        if(err) console.log(err);
+        else    console.log("inserted");
+    })
+})
+
+app.delete("/deleteToll/:delVal", (req,res)=>{
+    const tollName=req.params.delVal;
+
+    TollList.findOneAndDelete({name:tollName},(err)=>{
+        if(err) console.log(err)
+        else    console.log("Toll Deleted")
+    })
+})
 
 app.listen(PORT, ()=>{console.log("Server started ")});
