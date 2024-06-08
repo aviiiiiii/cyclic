2f5cf5105ae7e91cee1506328593565c22a88560
Aviiiiii <rockcena483@gmail.com>
2024-06-08 12:46:54 +0530

    cors1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 // app.use(cors());
-app.use(cors({ origin: ['http://localhost:5000', 'https://aviiiiii.site'], credentials: true }))
+app.use(cors({ origin: 'https://aviiiiii.site'}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
