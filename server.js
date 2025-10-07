d568464d708c56a422d3a9a8ed3882e3256fb022
aviiiiiii <76402441+aviiiiiii@users.noreply.github.com>
2025-10-07 12:39:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 // app.use(cors());
-app.use(cors({ origin: 'https://aviiiiiii.site'}))
+app.use(cors({ origin: '*'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
